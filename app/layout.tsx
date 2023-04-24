b736bd195770db3d3b909bddd6bfe9ea31c90e97
MarcinParda <marcin98parda@gmail.com>
2023-04-24 22:14:23 +0200

    refactor: remove boilerplate

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,15 @@ interface RootLayoutProps {
   children: React.ReactNode;
 }
 
+export const metadata = {
+  title: 'Just next ecommerce',
+  description:
+    'Welcome to my next Next.js project made with experimental appdir',
+};
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head />
       <body>{children}</body>
     </html>
   );
