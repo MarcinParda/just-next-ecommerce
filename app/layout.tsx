fa1a83f065dd0f2f0155a1d003cbc2f48ae042d3
MarcinParda <marcin98parda@gmail.com>
2023-04-24 22:43:53 +0200

    feat: installed chakra ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import { Roboto } from '@next/font/google';
+import { Providers } from '@/components/Providers/Providers';
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
@@ -8,10 +11,18 @@ export const metadata = {
     'Welcome to my next Next.js project made with experimental appdir',
 };
 
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700', '900'],
+  variable: '--font-roboto',
+  subsets: ['latin-ext'],
+});
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={roboto.variable}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
